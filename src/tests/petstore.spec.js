2e2c59ef5f687a7c301309791ab06cfdada8d147
kazimir-malevich <adrian_lewis@hushmail.com>
2022-12-28 20:27:56 +0000

    start petstore test

---
+++ src/tests/petstore.spec.js
@@ -0,0 +1,7 @@
+const { test, expect } = require("@playwright/test")
+
+test("Visit homepage and verify title", async ({ page }) => {
+  await page.goto("https://petstore.octoperf.com")
+
+  await expect(page).toHaveTitle("JPetStore Demo")
+})
