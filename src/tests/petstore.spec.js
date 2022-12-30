8f984ff0b5e39f20bc0ca96a5ead285ca98961c4
kazimir-malevich <adrian_lewis@hushmail.com>
2022-12-30 17:45:47 +0000

    rising above configuration hell

--- src/tests/petstore.spec.js
+++ src/tests/petstore.spec.js
@@ -2,6 +2,5 @@ const { test, expect } = require("@playwright/test")
 
 test("Visit homepage and verify title", async ({ page }) => {
   await page.goto("https://petstore.octoperf.com")
-
   await expect(page).toHaveTitle("JPetStore Demo")
 })
