bf01411c1447a4cdd81c9310225a213131038e26
kazimir-malevich <adrian_lewis@hushmail.com>
2023-01-02 15:04:55 +0000

    started to use page object model

--- src/tests/petstore.spec.js
+++ src/tests/petstore.spec.js
@@ -1,7 +1,26 @@
 const { test, expect } = require("@playwright/test")
 
+class HomePage {
+  constructor(page) {
+    this.page = page
+  }
+
+  async goto() {
+    await this.page.goto("https://petstore.octoperf.com")
+  }
+
+  async verifyTitle() {
+    await expect(this.page).toHaveTitle("JPetStore Demo")
+  }
+
+  async enterStore() {
+    await this.page.click("a[href='actions/Catalog.action']")
+  }
+}
+
 test("Buy a dog", async ({ page }) => {
-  await page.goto("https://petstore.octoperf.com")
-  await expect(page).toHaveTitle("JPetStore Demo")
-  await page.click("a[href='actions/Catalog.action']")
+  const homePage = new HomePage(page)
+  await homePage.goto()
+  await homePage.verifyTitle()
+  await homePage.enterStore()
 })
