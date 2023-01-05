4c4a265447dc61f58b4c58321778848f2c002fb3
kazimir-malevich <adrian_lewis@hushmail.com>
2023-01-05 11:38:28 +0000

    using a faker for test data

--- src/tests/RegisterPage.js
+++ src/tests/RegisterPage.js
@@ -1,25 +1,44 @@
+import { faker } from "@faker-js/faker"
+
 class RegisterPage {
   constructor(page) {
     this.page = page
   }
 
   async register() {
-    await this.page.locator('input[name="username"]').fill("username")
-    await this.page.locator('input[name="password"]').fill("password")
-    await this.page.locator('input[name="repeatedPassword"]').fill("password")
-    await this.page.locator('input[name="account\\.firstName"]').fill("aidy")
-    await this.page.locator('input[name="account\\.lastName"]').fill("bamber")
-    await this.page.locator('input[name="account\\.email"]').fill("x@x.com")
-    await this.page.locator('input[name="account\\.phone"]').fill("07551377577")
+    const password = faker.internet.password()
+    await this.page
+      .locator('input[name="username"]')
+      .fill(faker.internet.userName())
+    await this.page.locator('input[name="password"]').fill(password)
+    await this.page.locator('input[name="repeatedPassword"]').fill(password)
+    await this.page
+      .locator('input[name="account\\.firstName"]')
+      .fill(faker.name.firstName())
+    await this.page
+      .locator('input[name="account\\.lastName"]')
+      .fill(faker.name.lastName())
+    await this.page
+      .locator('input[name="account\\.email"]')
+      .fill(faker.internet.email())
+    await this.page
+      .locator('input[name="account\\.phone"]')
+      .fill(faker.phone.number())
     await this.page
       .locator('input[name="account\\.address1"]')
-      .fill("139 Kings Road")
-    await this.page.locator('input[name="account\\.city"]').fill("Manchester")
-    await this.page.locator('input[name="account\\.state"]').fill("MCR")
-    await this.page.locator('input[name="account\\.zip"]').fill("OL68EZ")
+      .fill(faker.address.streetAddress())
+    await this.page
+      .locator('input[name="account\\.city"]')
+      .fill(faker.address.city())
+    await this.page
+      .locator('input[name="account\\.state"]')
+      .fill(faker.address.stateAbbr())
+    await this.page
+      .locator('input[name="account\\.zip"]')
+      .fill(faker.address.zipCode())
     await this.page
       .locator('input[name="account\\.country"]')
-      .fill("United States")
+      .fill(faker.random.locale())
   }
 }
 
