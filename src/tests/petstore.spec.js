5d6405c696186abe4ae0b275a70824e16cf7c684
kazimir-malevich <adrian_lewis@hushmail.com>
2023-01-12 18:18:16 +0000

    click link by

--- src/tests/petstore.spec.js
+++ src/tests/petstore.spec.js
@@ -2,7 +2,12 @@ const { test, expect } = require("@playwright/test")
 
 test("Buy a dog", async ({ page }) => {
   await navigateToStore(page, "/", "JPetStore Demo", "Enter the Store")
+  await clickLinkBy(page, "Sign In")
 })
+
+async function clickLinkBy(page, name) {
+  await page.getByRole("link", { name: name }).click()
+}
 
 async function navigateToStore(page, url, expectedTitle, linkText) {
   // baseURL is set in the config
