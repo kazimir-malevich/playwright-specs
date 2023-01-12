d6c4cdd481c03dde051b9b3246d2235c9d68acf6
kazimir-malevich <adrian_lewis@hushmail.com>
2023-01-12 19:02:26 +0000

    no classes yeah

--- src/tests/petstore.spec.js
+++ src/tests/petstore.spec.js
@@ -1,9 +1,11 @@
+import { faker } from "@faker-js/faker"
 const { test, expect } = require("@playwright/test")
 
 test("Buy a dog", async ({ page }) => {
-  await navigateToStore(page, "/")
+  await navigateToStore(page)
   await clickLink(page, "Sign In")
   await clickLink(page, "Register Now")
+  await register(page)
 })
 
 async function clickLink(page, name) {
@@ -15,4 +17,34 @@ async function navigateToStore(page) {
   await page.goto("/")
   await expect(page).toHaveTitle("JPetStore Demo")
   await clickLink(page, "Enter the Store")
+}
+
+async function register(page) {
+  const password = faker.internet.password()
+  await page.locator('input[name="username"]').fill(faker.internet.userName())
+  await page.locator('input[name="password"]').fill(password)
+  await page.locator('input[name="repeatedPassword"]').fill(password)
+  await page
+    .locator('input[name="account\\.firstName"]')
+    .fill(faker.name.firstName())
+  await page
+    .locator('input[name="account\\.lastName"]')
+    .fill(faker.name.lastName())
+  await page
+    .locator('input[name="account\\.email"]')
+    .fill(faker.internet.email())
+  await page.locator('input[name="account\\.phone"]').fill(faker.phone.number())
+  await page
+    .locator('input[name="account\\.address1"]')
+    .fill(faker.address.streetAddress())
+  await page.locator('input[name="account\\.city"]').fill(faker.address.city())
+  await page
+    .locator('input[name="account\\.state"]')
+    .fill(faker.address.stateAbbr())
+  await page
+    .locator('input[name="account\\.zip"]')
+    .fill(faker.address.zipCode())
+  await page
+    .locator('input[name="account\\.country"]')
+    .fill(faker.random.locale())
 }
